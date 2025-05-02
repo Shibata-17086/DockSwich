93706dc63f00ee926a7f5a50cc59e0ca646c260f
koki <k.shiba30"gmail.com>
2025-05-02 21:10:00 +0900

    Initial Commit

---
+++ DockSwich/DockSwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockSwichApp.swift
+//  DockSwich
+//
+//  Created by 柴田紘希 on 2025/05/02.
+//
+
+import SwiftUI
+
+@main
+struct DockSwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DockSwichTests/DockSwichTests.swift
@@ -0,0 +1,17 @@
+//
+//  DockSwichTests.swift
+//  DockSwichTests
+//
+//  Created by 柴田紘希 on 2025/05/02.
+//
+
+import Testing
+@testable import DockSwich
+
+struct DockSwichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
